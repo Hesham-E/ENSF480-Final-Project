23cf48d9439797e4c547f3543fa42a68e8ac76fd
Hesham-E <hesham.elkaliouby@ucalgary.ca>
2021-12-03 16:18:03 -0700

    Removed reduntant comment from Observer

--- Model/Observer.java
+++ Model/Observer.java
@@ -3,7 +3,5 @@ package Model;
 import java.util.ArrayList;
 
 public interface Observer {
-    //should there be private variable subject here?
-    
     public void update(ArrayList<Property> catalog);
 }
