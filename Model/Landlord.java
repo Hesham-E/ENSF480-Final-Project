db99e2fdf1796ea948cffa8e862fd6d8c366607e
Maram Elsayed <maram.elsayed1@ucalgary.ca>
2021-12-04 12:19:39 -0700

    Still not complete calculating period is tricky

--- Model/Landlord.java
+++ Model/Landlord.java
@@ -7,12 +7,44 @@ public class Landlord implements Account{
     private ArrayList<Property> properties;
     private DBInterfaceController registrationController;
 
-    public void registerProperty () {
-
+    public Landlord() {} //incomplete
+    
+    public void registerProperty (Property p) {
+    	properties.add(p);
+    	payFee();
     }
 
     public void payFee () {
-
+    	feePayment fee = new feePayment();
+    	fee.pay(period,  amount); // incomplete
     }
     
+    public feePayment getFee() {
+        return this.fee;
+    }
+
+    public void setFee(feePayment fee) {
+        this.fee = fee;
+    }
+    
+    public ArrayList<Property> getProperties() {
+        return this.properties;
+    }
+
+    public void setProperties(ArrayList<Property> properties) {
+        this.properties = properties;
+    }
+    
+    public DBInterfaceController getRegistrationController() {
+        return this.registrationController;
+    }
+
+    public void setRegistrationController(DBInterfaceController registrationController) {
+        this.registrationController = registrationController;
+    }
 }
+
+/* Few Notes:
+- Not sure about payFee
+- Not sure of Properties setter and getter
+*/
--- Model/feePayment.java
+++ Model/feePayment.java
@@ -1,16 +1,27 @@
 package Model;
 
+import java.util.Date;
+
 public class feePayment {
     public boolean paid;
-    public Date timePeriod;
-    public double amount;
+    public static Date feePeriod;
+    public static double feeAmount;
+    
+    public feePayment() {} // incomplete
 
-    public void pay(boolean paid, double amount) {
-
+    public void pay(Date feePeriod, double feeAmount) { // changed it up
+    	
+    	this.feeAmount = feeAmount;
+    	this.paid = true;
+    	
+    	//incomplete
     }
-
-    public void periodCountdown(Date timePeriod) { //Date does not exist on diagram
-
+    
+    public void periodEnded(Date feePeriod) {
+    	Date currDate = new Date();
+        
+        if(currDate.getTime() = feePeriod.getTime()) 
+        	getNotified();
     }
 
     public Notifications getNotified() { //Notifications does not exist on diagram
