d4056460c3ea4ed2b9ed1152e76cec669d2257aa
Hesham-E <hesham.elkaliouby@ucalgary.ca>
2021-12-04 17:02:52 -0700

    Add Manager, Summary Report, and Account Classes
    User's members were changed to private. Additionally, Account has
    userInfo member added to it of type user so that all account classes
    that inehrit from account will have user information.

--- Model/User.java
+++ Model/User.java
@@ -1,9 +1,51 @@
 package Model;
 
 public class User {
-    public String username;
-    public String password;
-    public String name;
-    public String email;
-    public AccountType accountType;
+    //Changed all member types to private
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private AccountType accountType;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public AccountType getAccountType() {
+        return this.accountType;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+
 }
