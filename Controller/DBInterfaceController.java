ad61295131f27ba391bd17685a6fd21588d8cf88
alliana <alliana.dp@gmail.com>
2021-12-06 14:08:14 -0700

    finished DBInterfaceController

--- Controller/DBInterfaceController.java
+++ Controller/DBInterfaceController.java
@@ -1,5 +1,51 @@
 package Controller;
+import Model.*;
+import java.util.ArrayList;
 
-public interface DBInterfaceController {
+public abstract class DBInterfaceController {//changed from interface to abstract 
     protected Database database;
+
+    public DBInterfaceController(Database db){
+        this.database = db;
+        db.initializeConnection();
+    }
+
+    public void addDatabaseAccount(Account account){
+        database.addAccount(account);
+    }
+
+    public void getDatabaseAccountInfo(String username){
+        database.getAccountInfo(username);
+    }
+
+    public void addDatabaseProperty(Property property){
+        database.addProperty(property);
+    }
+
+    public Property getDatabaseProperty(int id){
+        Property p = database.getProperty(id);
+        return p;
+    }
+
+    public void updateDatabasePropertyState(Property property, String state){
+        database.updatePropertyState(property, state);
+    }
+
+    public void updateDatabaseProperty(Property property){
+        database.updateProperty(property);
+    }
+
+    public void deleteDatabaseProperty(int propertyID){
+        database.deleteProperty(propertyID);
+    }
+
+    public ArrayList<Property> getDatabasePropertyList(){
+        ArrayList<Property> propertyList = database.getPropertyList();
+        return propertyList;
+    }
+
+    public void updateDatabaseMessage(String message, Property property){
+        database.updateMessage(message, property);
+    }
+
 }
