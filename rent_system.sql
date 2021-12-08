22825a065507abf865fd54ad2b8d1675eb2da95d
alliana <alliana.dp@gmail.com>
2021-12-07 17:30:44 -0700

    Populated the tables

--- rent_system.sql
+++ rent_system.sql
@@ -14,10 +14,14 @@ CREATE TABLE ACCOUNTS (
 
 INSERT INTO ACCOUNTS (Username, Password, Name, Email, AccountType)
 VALUES
-('bear01',		'12345',	'Bear',		'bear@gmail.com',	 'RegRenter'),
-('turtle01',	'12345',	'Turtle',	'turtle@gmail.com',	 'Landlord'),
-('fox01',		'12345',	'Fox',		'fox@gmail.com',	 'Manager'),
-('cow01',		'12345',	'Cow',		'cow@gmail.com',	 'RegRenter');
+('thomasr',		'12345',	'Thomas Ross',			'thomas.r@gmail.com',	 'RegRenter'),
+('taylern',		'12345',	'Tayler Nielsen',		'tayler.n@gmail.com',	 'RegRenter'),
+('maureenr',	'12345',	'Maureen Reed',			'maureen.r@gmail.com',	 'Landlord'),
+('joannm',		'12345',	'Joann Merritt',		'joann.m@gmail.com',	 'Landlord'),
+('lucash',		'12345',	'Lucas Hughes',			'lucas.h@gmail.com',	 'Landlord'),
+('sophiav',		'12345',	'Sophia Villalobos',	'sohpia.v@gmail.com',	 'Landlord'),
+('jamalc',		'12345',	'Jamal Cope',			'jamal.c@gmail.com',	 'Landlord'),
+('aryaanr',		'12345',	'Aryaan Rhodes',		'aryaan.r@gmail.com',	 'Manager');
 
 
 DROP TABLE IF EXISTS PROPERTY;
@@ -28,7 +32,7 @@ CREATE TABLE PROPERTY (
     Address					varchar(200),
 	Type					varchar(25),
     BedroomNo				integer,
-    BathroomNo				double,
+    BathroomNo				integer,
     Furnished				varchar(25),
     CityQuad				char(2),
     Cost					double,
@@ -41,8 +45,26 @@ CREATE TABLE PROPERTY (
 
 INSERT INTO PROPERTY (HouseID, LandlordUsername, State, Address, Type, BedroomNo, BathroomNo, Furnished, CityQuad, Cost, DateListed, DateRented, FeeDueDate)
 VALUES
-(1,	'bear01',	'Active',	'242 Rainbow Road', 'Apartment',	3,	2.5, true, 'NW', 250000, '2021-12-01', null, '2022-01-30');
-
+(1,		'jamalc',	'SUSPENDED',	'242 Rainbow Road', 	'House',		4,	4, false, 	'NW', 2050, 	'2021-12-01', null, 		'2022-01-30'),
+(2,		'sophiav',	'SUSPENDED',	'39 Silver Street', 	'Condo',		2,	2, false, 	'NW', 1350, 	'2021-12-01', null, 		'2022-01-30'),
+(4,		'lucash',	'SUSPENDED',	'64 Wallow Way', 		'Duplex',		3,	1, false, 	'NW', 1400, 	'2021-12-01', null, 		'2022-01-30'),
+(5,		'joannm',	'CANCELLED',	'64 Wallow Way', 		'Duplex',		2,	2, false, 	'NW', 900, 		'2021-12-01', null, 		'2022-01-30'),
+(6,		'maureenr',	'CANCELLED',	'21 Butter Blvd', 		'Shared',		2,	4, false, 	'NW', 650, 		'2021-12-01', null, 		'2022-01-30'),
+(7,		'jamalc',	'RENTED',		'773 Rainbow Road', 	'Apartment',	2,	1, false, 	'NW', 1550, 	'2021-12-01', '2021-12-07', null),
+(8,		'sophiav',	'RENTED',		'3304 Wallow Way', 		'Apartment',	2,	2, false, 	'NW', 1195, 	'2021-12-01', '2022-12-06', null),
+(9,		'lucash',	'ACTIVE',		'463 Button Blvd', 		'Apartment',	1,	1, true, 	'NW', 790, 	  	'2021-12-01', null, 		'2022-01-30'),
+(10,	'joannm',	'ACTIVE',		'362 Silver Street', 	'Townhouse',	3,	1, true, 	'NE', 1250, 	'2021-12-01', null, 		'2022-01-30'),
+(11,	'maureenr',	'ACTIVE',		'3626 Rocky Road', 		'Apartment',	2,	1, true, 	'SW', 1680,   	'2021-12-01', null, 		'2022-01-30'),
+(12,	'jamalc',	'ACTIVE',		'463 Waffer Way', 		'Condo',		2,	1, true, 	'SE', 1400, 	'2021-12-01', null, 		'2022-01-30'),
+(13,	'sophiav',	'ACTIVE',		'463 Butter Blvd', 		'Apartment',	2,	1, false, 	'NW', 1050,	  	'2021-12-01', null, 		'2022-01-30'),
+(14,	'lucash',	'ACTIVE',		'63 Button Blvd', 		'House',		5,	4, false, 	'NW', 2995,		'2021-12-01', null, 		'2022-01-30'),
+(15,	'joannm',	'ACTIVE',		'82 Silver Street', 	'House',		3,	2, false, 	'NE', 1600, 	'2021-12-01', null, 		'2022-01-30'),
+(16,	'maureenr',	'ACTIVE',		'15 Rocky Road', 		'Shared',		1,	1, false, 	'SW', 600, 		'2021-12-01', null, 		'2022-01-30'),
+(17,	'lucash',	'ACTIVE',		'63 Button Blvd', 		'Duplex',		4,	2, false, 	'NW', 1800,		'2021-12-01', null, 		'2022-01-30'),
+(18,	'joannm',	'ACTIVE',		'82 Silver Street', 	'Duplex',		3,	1, false, 	'NE', 1250, 	'2021-10-08', null, 		'2021-12-07'),
+(19,	'maureenr',	'ACTIVE',		'15 Rocky Road', 		'Shared',		2,	1, false, 	'SW', 750, 		'2021-10-08', null, 		'2021-12-07'),
+(20,	'jamalc',	'ACTIVE',		'459 Button Blvd', 		'House',		3,	2, false, 	'SE', 1400, 	'2021-10-08', null,		    '2021-12-07');
+	
 DROP TABLE IF EXISTS MESSAGE;
 CREATE TABLE MESSAGE (
 	MessageID		int not null auto_increment,
@@ -53,5 +75,6 @@ CREATE TABLE MESSAGE (
 
 INSERT INTO MESSAGE (MessageID, LandlordEmail, Message)
 VALUES
-(1, "lan@gmail.com",	'Hello, I am interested in renting your property.');
+(1, 'jamal.c@gmail.com',	'Hello, I am interested in renting the condo at 459 Button Blvd SE. Please email me back at jaredf@yahoo.com'),
+(2, 'jamal.c@gmail.com',	'Hey, I am interested in renting the condo at 459 Button Blvd SE. Please email me back at lucyj@yahoo.com');
 
