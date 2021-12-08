b291f9808ed873486191056a32ed92f3aea8eda8
kdhawan2000 <77895143+kdhawan2000@users.noreply.github.com>
2021-12-08 02:47:15 -0700

    Update RegisterPropertyGUI.java

--- View/RegisterPropertyGUI.java
+++ View/RegisterPropertyGUI.java
@@ -8,7 +8,6 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 
