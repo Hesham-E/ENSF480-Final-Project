1a23ef2e3ee92359789110c874d1103021e3890f
kdhawan2000 <77895143+kdhawan2000@users.noreply.github.com>
2021-12-09 04:26:43 -0700

    Added a button to add property which redirects to RegisterPropertyGUI.java

--- View/LandlordAccountInfoGUI.java
+++ View/LandlordAccountInfoGUI.java
@@ -1,12 +1,14 @@
 package View;
 
 import Model.Property;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.*;
-import com.mysql.cj.x.protobuf.MysqlxNotice.Frame;
 
 
-public class LandlordAccountInfoGUI extends JFrame {
+public class LandlordAccountInfoGUI{
     private JLabel LandlordNameLabel;
     private JLabel LandlordUsernameLabel;
     private JLabel LandlordEmailLabel;
@@ -20,7 +22,9 @@ public class LandlordAccountInfoGUI extends JFrame {
     private JButton AddPropertyButton;
     private JLabel NumberOfPropertiesLabel;
     private ArrayList<Property> propertyList;
+    private static JButton extrab1;
 
+    
     public void setGUIUsername(String username){
         LandlordUsernameLabel.setText("Username: " + username);
     }
@@ -48,6 +52,30 @@ public class LandlordAccountInfoGUI extends JFrame {
     public JButton getSaveChangesButton(){
         return SaveChangesButton;
     }
+    
+    public LandlordAccountInfoGUI() {
+    
+    JFrame frame = new JFrame();
+	frame.setSize(400, 300);
+	frame.setTitle("Landlord Page!");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	JPanel lpanel = new JPanel();
+	frame.add(lpanel);
+	lpanel.setLayout(null);
+	
+	AddPropertyButton = new JButton("Register a new property!");
+	AddPropertyButton.setBounds(200,100,200,50);
+	AddPropertyButton.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent a){
+			frame.setVisible(false);
+			redirect.gotoaddpropbutton();
+	}
+	});
+	lpanel.add(AddPropertyButton);
+	frame.setVisible(true);
+    
+}
     
 }
 
