2a4e1b81523bb85438d8475ba319da5ec82935cb
alliana <alliana.dp@gmail.com>
2021-12-08 22:09:49 -0700

    Finished LandlordAccountController

--- Controller/LandlordAccountController.java
+++ Controller/LandlordAccountController.java
@@ -1,12 +1,43 @@
 package Controller;
 
+import Model.Landlord;
+import View.LandlordAccountInfoGUI;
+import Model.Property;
+
+import java.util.ArrayList;
+
+//Controls what information from database gets shown in the Landlord account page
 public class LandlordAccountController implements AccountController {
+    private Landlord theModel;
+    private LandlordAccountInfoGUI theView;
+
+    LandlordAccountController(Landlord model, LandlordAccountInfoGUI view){
+        this.theModel = model;
+        this.theView = view;
+
+        //When "Save Changes" button gets pressed, save new property info into database
+        theView.getSaveChangesButton().addActionListener (e -> {
+            updateDatabase();
+        });
+    }
+
+    //Sets the values to be shown for Landlord account info
     public void performGUIStrategy () {
-
+        theView.setGUIUsername(theModel.getUserInfo().getUsername());
+        theView.setGUIName(theModel.getUserInfo().getName());
+        theView.setGUIEmail(theModel.getUserInfo().getEmail());
+        theView.setGUIAccountType(theModel.getUserInfo().getAccountType().toString());
+        theView.setGUIProperties(theModel.getProperties());
     }
 
+
+    //Updates database on new property info changes
     public void updateDatabase () {
-        
+        ArrayList<Property> newPropertyList = theView.getGUIProperties();
+        for(int i = 0; i < newPropertyList.size(); i++){
+            database.updateProperty(newPropertyList.get(i));
+        }
     }
+
     
 }
