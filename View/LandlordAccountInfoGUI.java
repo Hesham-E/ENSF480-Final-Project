54c892184db1037b81e7235b967c8424cded7c4c
alliana <alliana.dp@gmail.com>
2021-12-08 20:05:38 -0700

    Added setters

--- View/LandlordAccountInfoGUI.java
+++ View/LandlordAccountInfoGUI.java
@@ -1,9 +1,16 @@
 package View;
 
+import Model.Property;
+import java.util.ArrayList;
 import javax.swing.*;
+import com.mysql.cj.x.protobuf.MysqlxNotice.Frame;
+
 
 public class LandlordAccountInfoGUI extends JFrame {
     private JLabel LandlordNameLabel;
+    private JLabel LandlordUsernameLabel;
+    private JLabel LandlordEmailLabel;
+    private JLabel LandlordAccountTypeLabel;
     private JTextField UserNameField;
     private JTextField PasswordField;
     private JTextField NameField;
@@ -12,4 +19,26 @@ public class LandlordAccountInfoGUI extends JFrame {
     private JButton ViewPropertiesButton;
     private JButton AddPropertyButton;
     private JLabel NumberOfPropertiesLabel;
+    private ArrayList<Property> propertyList;
+
+    public void setGUIUsername(String username){
+        LandlordUsernameLabel.setText("Username: " + username);
+    }
+
+    public void setGUIName(String name){
+        LandlordNameLabel.setText("Name: " + name);
+    }
+
+    public void setGUIEmail(String email){
+        LandlordEmailLabel.setText("Email: " + email);
+    }
+
+    public void setGUIAccountType(String type){
+        LandlordAccountTypeLabel.setText("Account Type: " + type);
+    }
+
+    public void setGUIProperties(ArrayList<Property> list){
+        this.propertyList = list;
+    }
 }
+
