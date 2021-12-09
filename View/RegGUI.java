c5dd2e018da04c2c5761b212fe19550dbc6497d7
kdhawan2000 <77895143+kdhawan2000@users.noreply.github.com>
2021-12-09 04:24:26 -0700

    Added a button to go back to login page

--- View/RegGUI.java
+++ View/RegGUI.java
@@ -1,12 +1,14 @@
-package view;
+package View;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
@@ -22,9 +24,11 @@ public class RegGUI{
 	private static JLabel confirmpasslabel;
 	private static JPasswordField confirmpass;
 	private static JButton regbutton;
+	private static JButton loginbutton;
 	private static JLabel usertype;
 	private static JComboBox usertypebox;
 	static String[] userTypes = {"Landlord","Renter"};
+	public boolean uniqueemail=true;
 	
 	public RegGUI() {
 		JFrame regframe = new JFrame();
@@ -71,21 +75,50 @@ public class RegGUI{
 	
         Object selection = usertypebox.getSelectedItem();
 		regbutton = new JButton("Register");
-		regbutton.setBounds(200,300,200,50);
+		regbutton.setBounds(200,220,200,50);
 		regbutton.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent a){
 				regframe.setVisible(false);
-				if(selection=="Landlord"){
-					redirect.backtologin();
+				if(useremail.getText().isEmpty()==true||uniqueemail==false){
+					JOptionPane.showMessageDialog(null, "Invalid email entered.", "Whoops", JOptionPane.ERROR_MESSAGE);
+					regframe.setVisible(true);
+				}
+				else if(pass.getPassword().length== 0){
+					JOptionPane.showMessageDialog(null, "Password cannot be empty.", "Whoops", JOptionPane.ERROR_MESSAGE);
+					regframe.setVisible(true);
+				}
+				else if (!Arrays.equals(pass.getPassword(), confirmpass.getPassword())) {
+					   JOptionPane.showMessageDialog(null, "Passwords do not match.", "Whoops", JOptionPane.ERROR_MESSAGE);
+					   regframe.setVisible(true);
+				} 
+				else {
+					System.out.println("valid");
+				    if(selection=="Landlord"){
+				    	JOptionPane.showMessageDialog(regframe, "Landlord account created! Kindly sign in now.");
+				    	redirect.backtologin();
+				    }
+				    else {
+				    	JOptionPane.showMessageDialog(regframe, "Reegistered renter account created! Kindly sign in now.");
+				    	redirect.backtologin();
+				    }
 				}
 				
 		}
 		});
 		registerpanel.add(regbutton);
 		
+		loginbutton = new JButton("Login instead?");
+		loginbutton.setBounds(200,300,200,50);
+		loginbutton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent a){
+				regframe.setVisible(false);
+				redirect.backtologin();
+		}
+		});
+		registerpanel.add(loginbutton);
 		
 		
 		regframe.setVisible(true);
 	}
-
+	
 }
