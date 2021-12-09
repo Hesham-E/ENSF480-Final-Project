341048cdb508ac53712886bc39e747257ec4cb19
kdhawan2000 <77895143+kdhawan2000@users.noreply.github.com>
2021-12-08 22:38:54 -0700

    Create redirect.java

---
+++ View/redirect.java
@@ -0,0 +1,12 @@
+package view;
+
+public class redirect {
+
+	public static void registeraccountbutton(){
+		new RegGUI();
+	}
+	
+	public static void backtologin(){
+		new RegisterPropertyGUI();
+	}
+}
