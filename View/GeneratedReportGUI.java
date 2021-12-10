cafae19c1f29a78e759990c0fadb4fd7620803b5
Hesham Elkaliouby <hesham.elkaliouby@ucalgary.ca>
2021-12-10 16:24:26 -0700

    Small Fixes

--- View/GeneratedReportGUI.java
+++ View/GeneratedReportGUI.java
@@ -63,7 +63,7 @@ public class GeneratedReportGUI {
 
         String list = "";
         for (int i = 0; i < rented.size(); i++ ){
-            list.append("Landlord: " + rented.get(i).getLandlord().getUserInfo().getName() + 
+            list += ("Landlord: " + rented.get(i).getLandlord().getUserInfo().getName() + 
                                 " House ID: " + rented.get(i).getHouseid() + " Address: " + rented.get(i).getAddress() + "\n");
         }
         count = new JLabel(list);
