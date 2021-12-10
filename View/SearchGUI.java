4613ed170d3e9fe8045e677aa310bbdd6f185d04
kdhawan2000 <77895143+kdhawan2000@users.noreply.github.com>
2021-12-10 04:00:31 -0700

    Update SearchGUI.java

--- View/SearchGUI.java
+++ View/SearchGUI.java
@@ -13,9 +13,13 @@ public class SearchGUI {
 	private static JComboBox bedbox;
 	private static JLabel bathlabel;
 	private static JComboBox bathbox;
+	private JLabel furnlabel;
+    private static JComboBox furnbox;
     private JLabel arealabel;
     private static JComboBox areabox;
     private JButton searchbutton;
+    private JButton loginbutton;
+    private JButton bookmarkbutton;
 
 	static String[] htypes = {"No preference","Apartment","Duplex","Condo","Basement",
 			                   "Loft","House","Townhouse","Shared"};
@@ -23,51 +27,65 @@ public class SearchGUI {
 	static String[] bed = {"No preference","Studio","1 Bed","1 + Den","2 Bed","2 + Den",
 			                   "3 Bed", "3 + Den","4+ Bed"};
 	static String[] bath = {"No preference","1 Bath","1.5 Bath","2 Bath","2.5 Bath","3+ Bath"};
-
+	static String[] options = {"No preference","Yes", "No" };
+	
     public SearchGUI(){
-
+//created a search frame 
         JFrame frame = new JFrame("Search");
         panel = new JPanel();
         frame.setSize(500, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
         panel.setLayout(null);
-
-        typelabel = new JLabel("Prefered house type:");
-        typelabel.setBounds(10, 80, 200, 25);
+//selection of preferred house type
+        typelabel = new JLabel("Preferred house type:");
+        typelabel.setBounds(10, 20, 200, 25);
         panel.add(typelabel);
 
         typebox = new JComboBox(htypes);
-        typebox.setBounds(250, 80, 110, 25);
+        typebox.setBounds(250, 20, 110, 25);
         panel.add(typebox);
 
-        bedlabel = new JLabel("Prefered number of bedrooms:");
-        bedlabel.setBounds(10, 110, 200, 25);
+//selection of preferred number of bedrooms
+        bedlabel = new JLabel("Preferred number of bedrooms:");
+        bedlabel.setBounds(10, 50, 200, 25);
         panel.add(bedlabel);
 
         bedbox = new JComboBox(bed);
-        bedbox.setBounds(250, 110, 110, 25);
+        bedbox.setBounds(250, 50, 110, 25);
         panel.add(bedbox);
+        
+//selection of preferred number of bathrooms
 
-        bathlabel = new JLabel("Prefered number of bathrooms:");
-        bathlabel.setBounds(10, 140, 200, 25);
+        bathlabel = new JLabel("Preferred number of bathrooms:");
+        bathlabel.setBounds(10, 80, 200, 25);
         panel.add(bathlabel);
 
         bathbox = new JComboBox(bath);
-        bathbox.setBounds(250, 140, 110, 25);
+        bathbox.setBounds(250, 80, 110, 25);
         panel.add(bathbox);
 
+//selection of preference in furnishing
 
+        furnlabel = new JLabel("Would you prefer a furnished house:");
+        furnlabel.setBounds(10, 110, 200, 25);
+        panel.add(furnlabel);
+
+        furnbox = new JComboBox(options);
+        furnbox.setBounds(250, 110, 110, 25);
+        panel.add(furnbox);
+
+//selection of preference in part of calgary
         arealabel = new JLabel("Prefered area of city:");
-        arealabel.setBounds(10, 170, 200, 25);
+        arealabel.setBounds(10, 140, 200, 25);
         panel.add(arealabel);
 
         areabox = new JComboBox(area);
-        areabox.setBounds(250, 170, 110, 25);
+        areabox.setBounds(250, 140, 110, 25);
         panel.add(areabox);
-
+//search button
         searchbutton = new JButton("Search");
-        searchbutton.setBounds(175, 270, 200, 50);
+        searchbutton.setBounds(175, 200, 200, 50);
 
         searchbutton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
@@ -80,6 +98,37 @@ public class SearchGUI {
             }
         });
         panel.add(searchbutton);
+
+        //back to login page
+        loginbutton = new JButton("Head back to login page");
+        loginbutton.setBounds(175, 260, 200, 50);
+
+        loginbutton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                frame.setVisible(false);
+                redirect.backtologin();
+            }
+        });
+        panel.add(loginbutton);
+        
+        //subscription button
+        bookmarkbutton = new JButton("Subscribe to this Search!");
+        bookmarkbutton.setBounds(10, 200, 150, 110);
+        bookmarkbutton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+//            	if(user is Registered renter) {
+                frame.setVisible(false);
+		    	JOptionPane.showMessageDialog(frame, "Subscription Saved!");
+            }
+//          }
+//            else {
+//            	frame.setVisible(false);
+//            	JOptionPane.showMessageDialog(null, "You must Login as Registered Renter to use this feature. Kindly Login and retry.", "Whoops", JOptionPane.ERROR_MESSAGE);
+//            	redirect.backtologin();
+//            }
+        });
+        panel.add(bookmarkbutton);
+        
 
         panel.setVisible(true);
         frame.setVisible(true);
